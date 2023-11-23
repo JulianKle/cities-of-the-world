a23d9f71496b86e8a6741858f8ad737be89aa47a
Julian Klett <julian22a@googlemail.com>
2023-11-23 15:35:01 +0100

    navigation and api for pictures

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     styledComponents: true,
   },
   reactStrictMode: true,
+  images: {
+    domains: ["d13k13wj6adfdf.cloudfront.net"],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
---
+++ pages/cities/[slug].js
@@ -0,0 +1,51 @@
+import { useRouter } from "next/router";
+import { cities } from "@/lib/data";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function DetailSite() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const [picture, setPicture] = useState("");
+
+  const city = cities.find((city) => city.slug === slug);
+
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch(
+        `https://api.teleport.org/api/urban_areas/slug:${slug}/images/`
+      );
+      const pictureObject = await response.json();
+      console.log(pictureObject.photos[0]?.image?.web);
+      setPicture(pictureObject.photos[0]?.image?.web);
+    }
+
+    startFetching();
+  }, []);
+
+  return (
+    <>
+      <h1>{city.name}</h1>
+      <h2>{city.country}</h2>
+      <ul>
+        Information:
+        <li>{city.description}</li>
+        <li>{city.population}</li>
+      </ul>
+
+      <Image
+        src={picture}
+        height={600}
+        width={600}
+        alt="A picture of a small dog"
+      ></Image>
+
+      <nav>
+        <Link href="/cities">Back to overviev</Link>
+      </nav>
+    </>
+  );
+}
